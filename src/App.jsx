a60b1bfdc6cdcd697432979b660e5877b6e18e6e
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-01-20 18:07:08 -0300

    implemented openAI image search

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, setPrompt] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    'Search a Star Wars battle in Brazil'
+  );
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const generateImage = async () => {
+    setPlaceholder(`Search ${prompt}..`);
+    setLoading(true);
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: '512x512',
+    });
+    setLoading(false);
+    setImageUrl(res.data.data[0].url);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='app-main'>
+      {loading ? (
+        <>
+          <h2>Generating image...</h2>
+        </>
+      ) : (
+        <>
+          <h2>Generate an image using Open AI API</h2>
+          <textarea
+            className='app-input'
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows='10'
+            cols='40'
+          />
+          <button onClick={generateImage}>Generate Image</button>
+          {imageUrl.length > 0 ? (
+            <img className='image-url' src={imageUrl} alt='Generated Image' />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
